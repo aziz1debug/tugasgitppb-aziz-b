1cf496d4ec2b68fa1625b780a7352f9539dbb578
aziz1debug <ajisktg906@gmail.com>
2026-09-18 17:48:31 +0700

    initial project

---
+++ praktikum_git.dart
@@ -0,0 +1,11 @@
+void main() {
+  int decimal = 10;
+  int octal = int.parse('12', radix: 8);
+  int binary = int.parse('1010', radix: 2);
+  int hexadecimal = 0xA;
+  print("ini punya aziz abdullah akbar");
+  print(decimal);
+  print(binary);
+  print(hexadecimal);
+  print(octal);
+}
